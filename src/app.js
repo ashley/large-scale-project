9b394b1ca089c59b6fedfd275a5e8ca494aabcb6
Ashley <msashleychen@gmail.com>
2018-11-27 17:58:17 -0500

    Added Google Map client

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const Tip = mongoose.model('Tip');
 const Rating = mongoose.model('Rating');
 
 const googleMapsClient = require('@google/maps').createClient({
-  key: 'your API key here'
+  key: '63c9bedefbc4066a842716ce75bd31c6a082dc4c'
 });
 
 // Front-End Wiring
