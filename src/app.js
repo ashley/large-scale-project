7f45de5bc8364efae1ffe6d21d34f3715ff4f2bd
Ashley Chen <msashleychen@gmail.com>
2018-12-14 15:44:55 -0500

    Remove google client since we use it all in the backend

--- src/app.js
+++ src/app.js
@@ -16,10 +16,6 @@ const Place = mongoose.model('Place');
 const Tip = mongoose.model('Tip');
 const Rating = mongoose.model('Rating');
 
-const googleMapsClient = require('@google/maps').createClient({
-  key: '63c9bedefbc4066a842716ce75bd31c6a082dc4c'
-});
-
 // Front-End Wiring
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
